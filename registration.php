4fafae349e68e4139d1fa439eabf3eb5c88977fb
nikolDi <37365459+nikolDi@users.noreply.github.com>
2019-03-19 22:48:04 +0200

    Update 19/03

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE, 
-	'Nikdimon_MaterialMagento2',
+	'Nikdimon_NikMaterialModule',
 	__DIR__
 );
